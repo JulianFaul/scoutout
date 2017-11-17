4cd5e44aa775dcf2bcfbd173ce32c629c5364be6
julianfaul <julianfaul3@gmail.com>
2017-11-17 12:46:29 +0200

    created new server

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const express       = require ('express')
+const bodyParser    = require ('body-parser')
+const morgan        = require ('morgan')
+const cors          = require ('cors')
+
+const app           = express()
+
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+app.use(cors())
+
+app.get('/status', (req,res) => {
+    res.send({
+        message: "hello world"
+    })
+})
+
+app.listen(process.env.PORT || 8081)
